14d458a99a48bb43986159dccf2801687aff37ec
Daniel Nascimento <danielcunha12.contato@gmail.com>
2024-05-16 17:42:28 -0300

    Update on program

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fb5bda002b5b4455408c8c40f0edcecb55dae33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+008e1fe9d104c633dd6d8250518c57762188a27a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
